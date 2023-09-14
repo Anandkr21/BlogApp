d16e63d8a2e27e0383ffd507f5ea7c6e87a8e417
Anandkr21 <anandkumargupta.akg15@gmail.com>
2023-09-14 15:08:52 +0530

    basic route created and working

---
+++ client/src/pages/About.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const About = () => {
+    return (
+        <div className='mb-20'>
+            <h1 className='sm:text-4xl text-2xl font-bold my-6 text-gray-900'>
+                About, welcome to my blog
+            </h1>
+            <p className='mx-auto leading-relaxe text-base mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste delectus sed asperiores obcaecati, molestias perferendis, illum nostrum labore optio, facere vero. Autem pariatur nobis qui quod. Aspernatur a quis quo nobis consequuntur, necessitatibus aut dolores iure recusandae beatae tenetur inventore ipsam officia eaque, enim qui eveniet. Adipisci iusto voluptas odit.</p>
+            <p className='mx-auto leading-relaxe text-base mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste delectus sed asperiores obcaecati, molestias perferendis, illum nostrum labore optio, facere vero. Autem pariatur nobis qui quod. Aspernatur a quis quo nobis consequuntur, necessitatibus aut dolores iure recusandae beatae tenetur inventore ipsam officia eaque, enim qui eveniet. Adipisci iusto voluptas odit.</p>
+            <p className='mx-auto leading-relaxe text-base mb-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste delectus sed asperiores obcaecati, molestias perferendis, illum nostrum labore optio, facere vero. Autem pariatur nobis qui quod. Aspernatur a quis quo nobis consequuntur, necessitatibus aut dolores iure recusandae beatae tenetur inventore ipsam officia eaque, enim qui eveniet. Adipisci iusto voluptas odit.</p>
+        </div>
+    )
+}
+
+export default About
